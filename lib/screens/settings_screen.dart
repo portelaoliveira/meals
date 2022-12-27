e0077eab186d6cc5dd345d3e8c22faf41fe55fae
Danilo Portela <daniloportela97@gmail.com>
2022-12-27 14:20:20 -0300

    Substituir telas.

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class SettingsScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text('Configurações'),
       ),
+      drawer: const MainDrawer(),
       body: const Center(
         child: Text('Configurações!'),
       ),
